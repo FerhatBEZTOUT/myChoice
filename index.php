8d60753137822e5434c6dcddbd883f44bdf35283
weghz <weghzan@gmail.com>
2022-05-22 17:04:11 +0100

    ferhat

--- index.php
+++ index.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="styles/index.css">
     <title>Document</title>
 </head>
 <body>
-    <?php
-    for ($i = 1; $i <= 10; $i++) {
-        echo $i;
-    }
-    ?>
+    <div class="container">
+        <div class="row justify-content">
+            <div class="col col-md-6">Bonjour</div>
+            <div class="col col-md-6">Bonjour</div>
+        </div>
+    </div>
 </body>
 </html>
